8380dccd9580e774ff4074b5ff7d50f6f1d7c939
Arttu Pakarinen <arttu.pakarinen@pp5.inet.fi>
2024-08-16 14:01:00 +0300

    some nice example data

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/styles.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'application/styles.css'));
 });
 
+app.get('/exampleData.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'application/exampleData.js'));
+});
+
 app.get('/asiat', (req, res) => {
     res.sendFile(path.join(__dirname, 'application/asiat.html'));
 });
---
+++ application/exampleData.js
@@ -0,0 +1,44 @@
+export const exampleData = [
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": -0.7
+    },
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": -0.5
+    },
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": -0.2
+    },
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": 0
+    },
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": 0.2
+    },  
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": 0.5
+    },
+    {
+        "id": 99999999,
+        "asia": "Kakka",
+        "seloste": 'Kakka',
+        "luokitus": 0.7
+    }
+]
