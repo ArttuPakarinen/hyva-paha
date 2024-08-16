45262ef7ff5175a9436851efa3ede985f28d9c1a
Arttu Pakarinen <arttu.pakarinen@pp5.inet.fi>
2024-08-16 14:31:01 +0300

    seloste -> selite

--- application/exampleData.js
+++ application/exampleData.js
@@ -2,43 +2,43 @@ export const exampleData = [
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": -0.7
     },
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": -0.5
     },
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": -0.2
     },
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": 0
     },
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": 0.2
     },  
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": 0.5
     },
     {
         "id": 99999999,
         "asia": "Kakka",
-        "seloste": 'Kakka',
+        "selite": 'Kakka',
         "luokitus": 0.7
     }
 ]
